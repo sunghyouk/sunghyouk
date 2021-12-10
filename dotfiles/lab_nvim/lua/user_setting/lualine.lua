08d0535716473f8ffb2aea6ccdc86eadd8c49f8b
sunghyouk <89435559+sunghyouk@users.noreply.github.com>
2021-12-10 22:45:23 +0900

    Create lualine.lua

---
+++ dotfiles/lab_nvim/lua/user_setting/lualine.lua
@@ -0,0 +1,33 @@
+local lualine = require('lualine')
+
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {"toggleterm", "NvimTree", "vista_kind"},
+    always_divide_middle = true,
+    extensions = {"fugitive"},
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff',
+                  {'diagnostics', sources={'nvim_diagnostic'}, colored = false}}, 
+-- Fix: un-colored the line_b because this option over-wrap to the identical color (6th, Dec, 2021)
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
