01f67b7217273b63079ee62942f08d01d124a97d
sunghyouk <sakebite@snu.ac.kr>
2022-02-21 15:29:50 +0900

    update configuration

--- dotfiles/nvim/lua/user_setting/sidebar.lua
+++ dotfiles/nvim/lua/user_setting/sidebar.lua
@@ -3,17 +3,17 @@ require("sidebar-nvim").setup({
     bindings = nil,
     open = false,
     side = "right", --default: left
-    initial_width = 50,
+    initial_width = 50, --default: 35
     hide_statusline = false,
     update_interval = 1000,
     sections = { "datetime", "git", "diagnostics", "todos", "buffers" },
-    section_separator = "-----",
+    section_separator = {"", "-----", ""},
     --containers = {
     --    attach_shell = "/bin/sh", show_all = true, interval = 5000,
     -- },
     datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
     todos = { ignored_paths = { "~" } },
-    --files = { show_hidden = false },
+    files = { show_hidden = false },
     symbols = { icon = "ƒ" },
     disable_closing_prompt = false
 })
