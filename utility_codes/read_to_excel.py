bc6d676807e48f3c893e589e2c8eca4481b689d5
sunghyouk <sakebite@snu.ac.kr>
2022-05-17 17:19:09 +0900

    solved: file name to CSV

--- utility_codes/read_to_excel.py
+++ utility_codes/read_to_excel.py
@@ -1,17 +1,33 @@
+import csv
 import os
 
 import pandas as pd
 
-file_list = os.listdir('/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library')
-file_save = []
+PATH = r'/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library/'
 
-for file in file_list:
-    file_sa = []
-    file_sa.append(file)
-    print(file)
+file_ls = []
+max_depth = 0
 
-    file_save.append(file_sa)
+for path, dir, files in os.walk(PATH):
+    for file in files:
+        current = os.path.join(path, file).replace('\\', '/')
+        file_ls.append(current)
 
-dataframe = pd.DataFrame(file_save)
-dataframe.to_csv("/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library/CS_pdf_book_list.csv",
-                 header=False, index=False)
+        if len(current.split('/')) > max_depth:
+            max_depth = len(current.split('/'))
+
+csv_path = '/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library'
+os.chdir(csv_path)
+depth = list(range(0, max_depth, 1))
+
+with open('book_list.csv', 'w', newline='', encoding='cp949') as f:
+    w = csv.writer(f)
+    w.writerow(' '.join(str(e) for e in depth).split())
+
+    for path, dir, files in os.walk(PATH):
+        for file in files:
+            current = os.path.join(path, file).replace('\\', '/')
+            row = os.path.relpath(current, PATH).split(os.sep)
+            w.writerow(row)
+
+df = pd.read_csv('book_list.csv', index_col=None, header=0, engine='python', encoding='cp949')
