e275da8a9f8f24cdfee6e90f3b13cd5ca2de2da7
sunghyouk <sakebite@snu.ac.kr>
2022-05-17 16:33:08 +0900

    not yet fixed

--- utility_codes/read_to_excel.py
+++ utility_codes/read_to_excel.py
@@ -1,37 +1,17 @@
 import os
-import pathlib
 
-import openpyxl
+import pandas as pd
 
+file_list = os.listdir('/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library')
+file_save = []
 
-def read_files(dir_path, filename):
+for file in file_list:
+    file_sa = []
+    file_sa.append(file)
+    print(file)
 
-    wb = openpyxl.Workbook()
-    ws = wb['Sheet']
-    xl_filename = filename + '.xlsx'
-    ws.cell(1, 1).value = 'Number'
-    ws.cell(1, 2).value = 'Name of Book'
-    wb.save('./' + xl_filename)
+    file_save.append(file_sa)
 
-    path = pathlib.Path(dir_path)
-    print(path)
-
-    files = os.listdir(path)
-    files_list_pdf = [file for file in files if file.endswith(".pdf")]
-    print(files_list_pdf)
-
-    row_cnt = 2
-    for file in files_list_pdf:
-        if file.is_file():
-            print(file.name)
-            ws.cell(row_cnt, 1).value = row_cnt - 1
-            ws.cell(row_cnt, 2).value = file.name
-            row_cnt += 1
-    wb.save('./' + xl_filename)
-    wb.close()
-
-
-directory = r'/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library'
-rfilename = 'CS_book_list'
-
-read_files(directory, rfilename)
+dataframe = pd.DataFrame(file_save)
+dataframe.to_csv("/Users/sunghyouk/Library/CloudStorage/OneDrive-개인/CS_library/CS_pdf_book_list.csv",
+                 header=False, index=False)
